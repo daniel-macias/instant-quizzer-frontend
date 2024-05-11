bf9b3b907eacb8fd990f13e2c68348737d17a4b0
daniel-macias <josedanielmr@gmail.com>
2024-05-10 23:52:37 -0600

    added share functionality with copy

--- app/share/[quizId]/page.tsx
+++ app/share/[quizId]/page.tsx
@@ -1,11 +1,39 @@
+"use client"
 import React from 'react';
 
-export default function ShareQuizPage ({params} : {params: {quizId: string}}) {
+interface ShareQuizPageProps {
+    params: {
+        quizId: string;
+    };
+}
+
+const ShareQuizPage: React.FC<ShareQuizPageProps> = ({ params }) => {
+    const quizLink = `http://localhost:3000/quiz/${params.quizId}`;
+
+    const handleCopyToClipboard = () => {
+        navigator.clipboard.writeText(quizLink).then(() => {
+            alert('Link copied to clipboard!'); 
+        }).catch(err => {
+            console.error('Failed to copy text: ', err);
+        });
+    };
 
     return (
         <div>
-            <h1>Share Quiz Page ID: {params.quizId}</h1>
-            <p>This page is a placeholder for the quiz with Page ID: {params.quizId}</p>
+            <h1>Congrats! Your quiz has been created successfully.</h1>
+            <p>Copy the following link and share it with someone else so they can take the quiz:</p>
+            <div>
+                <input
+                    type="text"
+                    value={quizLink}
+                    readOnly
+                    onClick={(event) => event.currentTarget.select()}
+                    className="w-full mb-4 p-2 border rounded text-black"
+                />
+                <button onClick={handleCopyToClipboard}>Copy to clipboard</button>
+            </div>
         </div>
     );
 };
+
+export default ShareQuizPage;
